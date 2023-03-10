add38d8cafa8168d224d6823bed30499ad8d9317
jocelinkisenga <kisengajocelin7@gmail.com>
2023-03-10 21:54:08 +0100

    added homework functionnalities

---
+++ database/migrations/2023_03_10_204916_create_homework_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHomeworkTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('homework', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('course_id');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('fichier')->nullable();
+            $table->boolean('deposer')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('homework');
+    }
+}
